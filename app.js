24eca526c063bf469307d1eee0537952b214970f
Kamil <kamilf20@gmail.com>
2019-01-08 00:05:16 +0100

    Adding CRUD

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var app = express();
 
 require('./services/cookieSession')(app);
-
+require('./models/product')
 require('./models/User');
 require('./services/passport');
 require('./services/mongoConnect');
@@ -17,6 +18,7 @@ require('./services/mongoConnect');
 
 require('./routes/googleAuth')(app);
 var indexRouter = require('./routes/index');
+var product = require('./routes/product'); // Imports routes for the products
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -30,6 +32,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/product',product);
 //app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
@@ -47,5 +50,6 @@ app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error');
 });
+
 
 module.exports = app;
